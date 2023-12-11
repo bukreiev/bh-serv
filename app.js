ca6106a0c5a90d6534a1e44e2bac28cdd4717c18
Hennadii Bukreiev <bukreiev@ukr.net>
2023-12-12 00:25:03 +0200

    fix cors set 2

--- app.js
+++ app.js
@@ -9,30 +9,23 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: '*',
-  })
-);
+app.use(cors());
 sgMail.setApiKey(process.env.SG_KEY);
 
 app.post('/send-email', async (req, res) => {
   const { name, email, message } = req.body;
 
-  // Настройки для отправки почты (замените на свои данные)
-
-  // Настройка письма
   const msg = {
-    to: TO, // Ваш email для получения сообщений
-    from: FROM, // Адрес отправителя
+    to: TO,
+    from: FROM,
     subject: 'Новое сообщение с формы',
     text: `Имя: ${name}\nEmail: ${email}\nСообщение: ${message}`,
   };
 
-  // Отправка почты
   try {
     await sgMail.send(msg);
     console.log('Email отправлен успешно');
+    res.header('Access-Control-Allow-Origin', '*');
     res.status(200).send('Email отправлен успешно');
   } catch (error) {
     console.error(error);
